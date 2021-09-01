952d8d48cf96963260150cf1830b7d971e38361b
Joseph <josephtecson18@gmail.com>
2021-08-31 21:30:10 -0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 class="color">Hello Dojo!</h1>
+        <h3>Things I need to do:</h3>
+        <p>* Learn 3/4 of React's Backbone</p>
+        <p>* Climb this Mt. Everest of a MERN stack</p>
+        <p>* Run a marathon of all-nighters learning React</p>
+        <p>* Feed the dogs JSX</p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
